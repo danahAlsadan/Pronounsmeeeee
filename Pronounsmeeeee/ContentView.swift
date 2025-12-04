03c6e76b206393a708e92ab51bffb20ccba597c6
danah <ddaana0220@gmail.com>
2025-12-04 17:40:59 +0300

    Initial Commit

---
+++ Pronounsmeeeee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pronounsmeeeee
+//
+//  Created by danah alsadan on 13/06/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pronounsmeeeee/PronounsmeeeeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PronounsmeeeeeApp.swift
+//  Pronounsmeeeee
+//
+//  Created by danah alsadan on 13/06/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct PronounsmeeeeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
